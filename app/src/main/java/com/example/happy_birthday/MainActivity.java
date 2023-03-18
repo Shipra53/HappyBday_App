714709f47523ede71a689ad56c86d354aa542d78
Shipra53 <upadhyayshipra84@gmail.com>
2023-03-18 21:15:56 +0530

    ninth app commit

---
+++ app/src/main/java/com/example/happy_birthday/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.happy_birthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("First"," HAPPY BIRTHADAY");
+                Toast.makeText(MainActivity.this,"Hope Your Birthday Is As Wonderful As You Are!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
